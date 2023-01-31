4ebb8bdc9b3aa3514dbafad7eb90428177103959
chaicoffee08 <chaithanya.jade@gmail.com>
2023-01-31 11:39:51 +0530

    Initial Commit

---
+++ Tindog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tindog
+//
+//  Created by CJ on 31/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
